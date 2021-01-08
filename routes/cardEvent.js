e23dca38c199f3f4d46054571e0a2330d5deec77
Nickamg <nicolasmagallon.design@gmail.com>
2021-01-08 22:14:44 +0100

    Cambio desde movil 📱

--- routes/cardEvent.js
+++ routes/cardEvent.js
@@ -4,6 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log(JSON.stringify(req));
+  console.log('ok');
 });
 
 module.exports = router;
